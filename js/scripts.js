018fb4fb587404a4eb969905aeabc5fccfcd3be4
Okall Vivian <vivieokall@gmail.com>
2018-12-10 09:56:56 +0300

    Update index.html and Styles.css

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var answers=[]
+     scores=0;
+  questions=["ans","ans","ans","ans","ans"];
+
+functions sum(){
+  scores = answers[0]+answer[1]+answer[2]+answer[3]+answer[4]
+  return points;
+}
+
+$(document).ready(function){
+  
+}
